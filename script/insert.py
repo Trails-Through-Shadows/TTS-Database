650d1c59ab49981d5a284d232fdc552c0fbe3187
Firestone82 <mikpav@email.cz>
2024-01-29 22:32:13 +0100

    Added auto remove .sql files before insert
    Took 4 minutes

--- script/insert.py
+++ script/insert.py
@@ -12,6 +12,20 @@ if not (len(sys.argv) > 1 and sys.argv[1] == '-s'):
 insertFolder = 'insert'
 
 log("", "INFO", True)
+log("Removing all the .sql files in the directory...", "INFO", True)
+
+# Remove all .sql files in directory
+for dirPath, _, fileNames in os.walk(insertFolder):
+    for fileName in fileNames:
+        filePath = os.path.join(dirPath, fileName)
+
+        # SQL files
+        if fileName.lower().endswith('.sql') and fileName.lower() != '0-license.sql':
+            log(" - Removing " + filePath + "...", "INFO", True)
+            os.remove(filePath)
+            
+log("Successfully removed all the .sql files in the directory.", "INFO", True)
+log("", "INFO", True)
 log("Executing all the Python files...", "INFO", True)
 
 # Execute all the Python files

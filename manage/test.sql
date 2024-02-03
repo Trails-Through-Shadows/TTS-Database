5a4f826535cb9b852d8a927b819329031d19dd47
Firestone82 <mikpav@email.cz>
2024-02-03 23:00:24 +0100

    Cleanup
    Took 8 minutes

--- manage/test.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM Part WHERE id = 1;
--- manage/trigger.sql
+++ manage/trigger.sql
@@ -1,3 +1,5 @@
+# Trigger on Part relation update
+
 CREATE OR REPLACE TRIGGER trg_UpdatePartRelationsINSERT
     AFTER INSERT ON LocationPart
     FOR EACH ROW
@@ -14,4 +16,44 @@ BEGIN
     UPDATE Part
     SET usages = COALESCE((SELECT COUNT(idLocation) FROM LocationPart WHERE id = OLD.idPart GROUP BY idPart), 0)
     WHERE id = OLD.idPart;
+END;
+
+# Trigger on Enemy relation update
+
+CREATE OR REPLACE TRIGGER trg_UpdateEnemyRelationsINSERT
+    AFTER INSERT ON HexEnemy
+    FOR EACH ROW
+BEGIN
+    UPDATE Enemy
+    SET usages = COALESCE((SELECT COUNT(idLocation) FROM HexEnemy WHERE idEnemy = NEW.idEnemy GROUP BY idEnemy), 0)
+    WHERE id = NEW.idEnemy;
+END;
+
+CREATE OR REPLACE TRIGGER trg_UpdateEnemyRelationsDELETE
+    AFTER DELETE ON HexEnemy
+    FOR EACH ROW
+BEGIN
+    UPDATE Enemy
+    SET usages = COALESCE((SELECT COUNT(idLocation) FROM HexEnemy WHERE id = OLD.idEnemy GROUP BY idEnemy), 0)
+    WHERE id = OLD.idEnemy;
+END;
+
+# Trigger on Obstacle relation update
+
+CREATE OR REPLACE TRIGGER trg_UpdateObstacleRelationsINSERT
+    AFTER INSERT ON HexObstacle
+    FOR EACH ROW
+BEGIN
+    UPDATE Obstacle
+    SET usages = COALESCE((SELECT COUNT(idLocation) FROM HexObstacle WHERE idObstacle = NEW.idObstacle GROUP BY idObstacle), 0)
+    WHERE id = NEW.idObstacle;
+END;
+
+CREATE OR REPLACE TRIGGER trg_UpdateObstacleRelationsDELETE
+    AFTER DELETE ON HexObstacle
+    FOR EACH ROW
+BEGIN
+    UPDATE Obstacle
+    SET usages = COALESCE((SELECT COUNT(idLocation) FROM HexObstacle WHERE id = OLD.idObstacle GROUP BY idObstacle), 0)
+    WHERE id = OLD.idObstacle;
 END;
